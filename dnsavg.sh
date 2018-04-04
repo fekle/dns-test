61cd2afdf4a3e602c018bc616e0d8e0635914acb
Felix Klein <felix@felixklein.net>
2018-04-05 00:15:23 +0200

    no commit message

--- dnsavg.sh
+++ dnsavg.sh
@@ -6,7 +6,8 @@ tlds=(com org co.uk net ca de jp fr au us at ch it nl io)
 
 # get a random 64 char hex string
 function random_hex() {
-  head -c 128 /dev/urandom | base64 | fold -w1 | shuf | grep -Eo '[a-Z0-9]' | tr -d '\n' | tr '[:upper:]' '[:lower:]' | cut -c-64
+  head -c 128 /dev/urandom | base64 | \
+    fold -w1 | shuf | grep -Eo '[a-Z0-9]' | tr -d '\n' | cut -c-64 | tr '[:upper:]' '[:lower:]'
 }
 
 # get a random domain
@@ -30,9 +31,9 @@ printf 'testing %s domains on %s:%s\n' "${rounds}" "${server}" "${port} with ${c
 
 # do the magic
 addition=''
-for _ in $(seq 1 "${rounds}"); do
+for i in $(seq 1 "${rounds}"); do
   domain=$(random_domain)
-  time=$(${cmd} -p "${port}" "${domain}" IN A "@${server}" | grep -Eo ';; Query time: [0-9]+(\.[0-9]+)? msec' | grep -Eo '[0-9]+(\.[0-9]+)?')
+  time=$(${cmd} -p "${port}" "${domain}" IN A "@${server}" | grep -Eo ';; Query time: [0-9]+(\.[0-9]+)? msec' | grep -Eo '[0-9]+(\.[0-9]+)?') || { echo 'query failed'; exit 1; }
   addition="${addition} + ${time}"
   sleep "0.0$(shuf -i 01-25 -n 1)" # let's not go crazy
 
